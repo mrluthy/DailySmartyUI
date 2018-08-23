284abc0f20218beee34ae4958c42c2c63dc96adb
Michael Luthy <you@example.com>
2018-08-23 15:47:55 -0600

    setup logo and corresponding styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Logo from './logo'
+import SearchBar from './searchBar'
+import RecentPosts from './recentPosts'
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Logo</h1>
-        <h2>React Redux Router</h2>
+        <Logo />
+        <SearchBar />
+        <RecentPosts />
       </div>
     );
   }
---
+++ src/components/logo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Logo extends Component {
+  render() {
+    return (
+      <div className="logo-main">
+        <img src="\assets\image11.png" alt="Daily Smarty UI Image Logo Big"/>
+
+      </div>
+    )
+  }
+}
+
+export default Logo;
